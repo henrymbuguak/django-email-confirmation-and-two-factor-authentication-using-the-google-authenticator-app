ad6e09a2afffc0d7b99a60a14e49386c67fe1935
henrymbuguak <henrymbuguak@gmail.com>
2018-08-19 20:30:19 +0300

    change on email configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,8 +126,8 @@ STATIC_URL = '/static/'
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'inquiries@reportlegend.com'
-EMAIL_HOST_PASSWORD = 'ReportLegendpl@y'
+EMAIL_HOST_USER = 'henrymbuguak@gmail.com'
+EMAIL_HOST_PASSWORD = '3918km1990hmk'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
